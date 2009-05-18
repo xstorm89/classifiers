835bc25bc8741295b345204f6753769f5263084e
amfaheem <amfaheem@f558889a-2cd2-11de-8e0c-53a27eea117e>
2009-05-18 15:34:35 +0000

    added DHB algorithm. needs refacatoring before adding the other related algorithms (DHF, AFB, ABF)

---
+++ clustering/src/org/pr/clustering/IterativeMinimumSquareError.java
@@ -0,0 +1,112 @@
+package org.pr.clustering;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.pr.clustering.util.DoubleUtils;
+
+public class IterativeMinimumSquareError extends KMeans {
+
+	public IterativeMinimumSquareError(int k, int dims, String filename, String delimiter) {
+		super(k, dims, filename, delimiter);
+	}
+	
+	public IterativeMinimumSquareError(int k, Vector[] patterns) {
+		super(k, patterns);
+	}
+	
+	@Override
+	public List<Integer> partition() {
+		List<Vector> Z = new ArrayList<Vector>();
+		for (int i = 0; i < k; i++) {
+			int index = ( patterns.length * (i) ) / k;
+			Z.add(patterns[index]);
+		}
+		
+		double newGlobalObjFun = getMeanSquareError(Z);
+		double oldGlobalObjFun;
+		int numberOfStableIterations = 0;
+		for (int m = 0; ; m++) {
+			oldGlobalObjFun = newGlobalObjFun;
+			
+			// for each pattern, find its cluster
+			// then calc the gain of moving it out of that cluster
+			// and the cost of moving it to any of the other clusters
+			for (int l = 0; l < patterns.length; l++) {
+				int i = mm.getClusterForPattern(l);
+				int[] iPatterns = mm.getPatternsForCluster(i);
+				if (iPatterns.length > 1) {
+					double gain = (iPatterns.length / (iPatterns.length - 1))
+						* Vector.euclideanDistance(patterns[l], Z.get(i));
+					
+					double minCost = Double.MAX_VALUE;
+					int clusterToMoveTo = -1;
+					// search for a cluster to move this pattern to
+					// to decrease the objective function
+					for (int j = 0; j < k; j++) {
+						if (j != i) {// exclude ith cluster that we are trying to take a pattern from
+							int[] otherClusterPatterns = mm.getPatternsForCluster(j);
+							double cost = (((double)otherClusterPatterns.length) / (double)(otherClusterPatterns.length + 1))
+								* Vector.euclideanDistance(patterns[l], Z.get(j));
+							if (cost < gain) { // there's an improvement, but is this is the best improvement
+								if (cost < minCost) { // better than all before
+									minCost = cost;
+									clusterToMoveTo = j;
+								}
+							}
+						}
+					}
+					if (clusterToMoveTo > -1) { // move pattern i to cluster clusterToMoveTo 
+						mm.movePattern(l, clusterToMoveTo);
+						updateZ(Z, i, clusterToMoveTo);
+					}
+				}
+			}
+			
+			newGlobalObjFun = getMeanSquareError(Z);
+			// if the objective function is the same
+			// advance stability count
+			if (DoubleUtils.equal(newGlobalObjFun, oldGlobalObjFun)) {
+				numberOfStableIterations++;
+				if (numberOfStableIterations == patterns.length) { // we're done
+					break;
+				}
+			} else
+				numberOfStableIterations = 0; // reset, stability has been broken
+		}
+		
+		cluserCenters = Z;
+		
+		return mm.getClusters();	
+	}
+	
+	public static void main(String[] args) { 
+//		Vector v1 = new Vector(0, 0);
+//		Vector v2 = new Vector(1.5, 1.5);
+//		Vector v3 = new Vector(1, 1);
+//		Vector v4 = new Vector(5, 5);
+//		Vector v5 = new Vector(5.5, 5.5);
+//		Vector v6 = new Vector(6, 6);
+//		Vector v7 = new Vector(100, 100);
+//		Vector v8 = new Vector(105, 105);
+//		Vector v9 = new Vector(102, 102);
+//		
+//		KMeans kmeans = new KMeans(3, v1, v5, v4, v7, v9, v8, v2, v3, v6);
+		
+//		KMeans kmeans = new KMeans(2, 2, "C:/Gaussian.in", "\t");
+//		List<Integer> clusters = kmeans.partition();
+		
+		IterativeMinimumSquareError DHF = new IterativeMinimumSquareError(2, 2, "C:/Gaussian.in", "\t");
+		long startTime = Calendar.getInstance().getTimeInMillis();
+		List<Integer> DHFClusters = DHF.partition();
+		long endTime = Calendar.getInstance().getTimeInMillis();
+		System.out.println("ExecutionTime = " + (endTime - startTime) / 1000 + " sec");
+		 
+//		System.out.println("KMeans-----------------");
+//		System.out.println(kmeans.printResults());
+		System.out.println("DHF--------------------");
+		System.out.println(DHF.printResults());
+	}
+	
+}
