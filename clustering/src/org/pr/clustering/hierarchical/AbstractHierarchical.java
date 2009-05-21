8ce995d9bbeeceeed22afa36ebf84e0cf306638e
amfaheem <amfaheem@f558889a-2cd2-11de-8e0c-53a27eea117e>
2009-05-21 23:37:34 +0000

    basic datatypes for hierarchical clustering.

---
+++ clustering/src/org/pr/clustering/hierarchical/AbstractHierarchical.java
@@ -0,0 +1,20 @@
+package org.pr.clustering.hierarchical;
+
+import java.util.List;
+
+import org.pr.clustering.Vector;
+
+/**
+ * @author Ahmad Faheem & Amr Ghoneim 
+ */
+public class AbstractHierarchical {
+
+	Vector[] patterns;
+	
+	public List<Integer> partition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
